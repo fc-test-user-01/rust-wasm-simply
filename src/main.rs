95a6f010320983e71fce49fd47d5945aeef5bf7f
root <r.kobuki@firstconnect.co.jp>
2023-11-14 18:31:13 +0900

    m

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-// src/lib.rs
+use std::ffi::{CString, CStr};
+use std::os::raw::c_char;
+use std::ptr;
 
 // Define a global variable to store a value
 static mut MY_VALUE: i32 = 0;
@@ -17,6 +19,17 @@ pub extern "C" fn get_value() -> i32 {
     unsafe {
         MY_VALUE
     }
+}
+
+#[no_mangle]
+pub extern "C" fn get_string(ptr: *mut c_char) -> *const c_char {
+    if ptr.is_null() {
+        return ptr::null();
+    }
+    let c_str = unsafe { CStr::from_ptr(ptr) };
+    let rust_string = format!("Rust says: {}", c_str.to_string_lossy());
+    let new_c_string = CString::new(rust_string).expect("Failed to create CString");
+    new_c_string.into_raw()
 }
 
 #[no_mangle]
